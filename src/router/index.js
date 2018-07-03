91701758fa9c410fe0130ec323516f494bffbdf6
binge0-0 <361204134@qq.com>
2018-07-03 14:24:25 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import ren from '@/components/ren'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/ren',
+      name: 'ren',
+      component: ren
     }
   ]
 })
